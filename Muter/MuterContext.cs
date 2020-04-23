e60fa520527ad155965d8459d635ca19a3f3906d
Adam Guscott <adguscott@gmail.com>
2020-04-23 12:36:09 +0100

    Add project files.

---
+++ Muter/MuterContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Muter
+{
+    class MuterContext : ApplicationContext
+    {
+        NotifyIcon notifyIcon;
+        MuterTimer muter;
+        public MuterContext()
+        {
+            muter = new MuterTimer();
+            MenuItem startMenuItem = new MenuItem("Start", new EventHandler(Start));
+            MenuItem stopMenuItem = new MenuItem("Stop", new EventHandler(Stop));
+            MenuItem closeMenuItem = new MenuItem("Close", new EventHandler(Close));
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Muter.Properties.Resources.icon;
+            notifyIcon.ContextMenu = new ContextMenu(new MenuItem[]
+                { startMenuItem, stopMenuItem, closeMenuItem });
+            notifyIcon.Visible = true;
+        }
+
+        public  void Start(object sender, EventArgs e)
+        {
+            muter.Start();
+        }
+
+        public void Stop(object sender, EventArgs e)
+        {
+            muter.Stop();
+        }
+
+        public void Close(object sender, EventArgs e)
+        {
+            notifyIcon.Icon = null;
+            Application.Exit();
+        }
+    }
+}
---
+++ Muter/MuterTimer.cs
@@ -0,0 +1,52 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Muter
+{
+    class MuterTimer
+    {
+        Timer timer;
+        CoreAudioDevice defaultPlaybackDevice;
+        public MuterTimer()
+        {
+            defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+        }
+
+        public void Start()
+        {
+            timer = new Timer(60000);
+            timer.Elapsed += Mute;
+            timer.Enabled = true;
+        }
+
+        public void Stop()
+        {
+            if (timer.Enabled)
+                timer.Dispose();
+            UnMute();
+        }
+
+        public bool GetMuteStatus()
+        {
+            return defaultPlaybackDevice.IsMuted;
+        }
+
+        public void Mute(object sender, ElapsedEventArgs e)
+        {
+            if (!GetMuteStatus())
+                defaultPlaybackDevice.Mute(true);
+        }
+
+        public void UnMute()
+        {
+            if (GetMuteStatus())
+                defaultPlaybackDevice.Mute(false);
+        }
+    }
+}
